636df8c47d562b4a80dce5b50506f65d2f4ce318
Felipe Hertzer <felipeaugustohertzer@gmail.com>
2016-08-19 12:38:26 -0300

    Initial commit

---
+++ app/Pessoa.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Pessoa extends Authenticatable
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nome', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
