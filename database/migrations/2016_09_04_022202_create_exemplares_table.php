14f4220cc919c3fd14d6ef67534b28e4437d5c35
Willian Serafini <willian@onehost.com.br>
2016-09-03 23:30:51 -0300

    adicionando migration de exemplares

---
+++ database/migrations/2016_09_04_022202_create_exemplares_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateExemplaresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exemplares', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('estante');
+            $table->integer('prateleira');
+            $table->enum('status', ['E', 'D']);
+            $table->boolean('danificado');
+            $table->integer('livro_id')->unsigned();
+            $table->foreign('livro_id')->references('id')->on('livros');
+            $table->integer('tipoexemplar_id')->unsigned();
+            $table->foreign('tipoexemplar_id')->references('id')->on('tipoexemplares');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('exemplares');
+    }
+}
