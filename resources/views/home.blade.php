8aaaf3b4ce47f96c475ff07aeb3df216eb2819b9
Willian Serafini <willian@onehost.com.br>
2016-11-24 22:41:24 -0200

    adicionando validacoes na home das permissoes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,11 +11,25 @@
                         <h3 style="margin-top:0px;">Seja bem-vindo ao sistema de gerenciamento escolar</h3>
 
                         <div class="list-group">
-                            <a href="{{ url('/admin/pessoas') }}" class="list-group-item"><i class="fa fa-user-o"></i> Cadastrar Pessoa</a>
-                            <a href="{{ url('/admin/matriculas') }}" class="list-group-item"><i class="fa fa-user"></i> Realizar Matricula</a>
-                            <a href="{{ url('/admin/biblioteca/retiradas') }}" class="list-group-item"><i class="fa fa-btn fa-reply"></i>Retirada</a>
-                            <a href="{{ url('/admin/biblioteca/reservas') }}" class="list-group-item"><i class="fa fa-clock-o"></i> Reserva</a>
-                            <a href="{{ url('/admin/emprestamateriais') }}" class="list-group-item"><i class="fa fa-share-square-o"></i> Emprestar Material</a>
+                            @if(App\Lib\Permissoes::podeExibirMenu('pessoas'))
+                                <a href="{{ url('/admin/pessoas') }}" class="list-group-item"><i class="fa fa-user-o"></i> Cadastrar Pessoa</a>
+                            @endif
+                            
+                            @if(App\Lib\Permissoes::podeExibirMenu('matriculas'))
+                                <a href="{{ url('/admin/matriculas') }}" class="list-group-item"><i class="fa fa-user"></i> Realizar Matricula</a>
+                            @endif
+                            
+                            @if(App\Lib\Permissoes::podeExibirMenu('retiradas'))
+                                <a href="{{ url('/admin/biblioteca/retiradas') }}" class="list-group-item"><i class="fa fa-btn fa-reply"></i>Retirada</a>
+                            @endif
+                            
+                            @if(App\Lib\Permissoes::podeExibirMenu('reservas'))
+                                <a href="{{ url('/admin/biblioteca/reservas') }}" class="list-group-item"><i class="fa fa-clock-o"></i> Reserva</a>
+                            @endif
+                            
+                            @if(App\Lib\Permissoes::podeExibirMenu('emprestamateriais'))
+                                <a href="{{ url('/admin/emprestamateriais') }}" class="list-group-item"><i class="fa fa-share-square-o"></i> Emprestar Material</a>
+                            @endif
                         </div>
                     </div>
                 </div>
